8f28d67e8e2a61a4a68889b47de2a61646114a02
Anton Alexeev <alexeev.sker@gmail.com>
2018-04-02 00:20:15 +0300

    Add connect and disconect event handlers

--- server/server.js
+++ server/server.js
@@ -1,27 +1,33 @@
 require('./config/config');
 
 const path = require('path');
-const publicPath = path.join(__dirname,'..','public');
+const http = require('http');
 const express = require('express');
-const fs = require('fs');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT;
 
 const app = express();
-
-const port = process.env.PORT;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (res,req) => {
-//     let body = fs.readFileSync(path.join(publicPath,'index.html'));
-//     req.send(body.toString());
-//     req.setHeader('Content-type:text/html');
-// });
+io.on('connection', (socket) => {
+  console.log('New User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User dsiconnect');
+  });
+
+});
 
 if(!module.parent){
-    app.listen(port, () => {
-      console.log(`Started up at port ${port}`);
-    });
-  }
+  server.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+  });
+}
   
-  module.exports = { app };
+module.exports = { app };
   
